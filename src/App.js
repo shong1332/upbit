70e1581d8df382a662a95c8eb564f016510a0023
김순홍 <kimsoonhong@gimsunhong-ui-MacBookPro.local>
2024-01-30 22:30:14 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import axios from 'axios';
+import { v4 as uuidv4 } from "uuid";
+import jwt from "jsonwebtoken";
+
 
 function App() {
+
+
+
+   const getBTCExchangeRate = async () => {
+
+    try {
+      const response = await axios.get(
+  
+        'https://api.upbit.com/v1/ticker?markets=KRW-BTC',
+      );
+      return response.data[0].trade_price;
+    } catch (error) {
+      console.error('Error fetching Polygon exchange rate:', error);
+      return false;
+    }
+  };
+  
+  
+  
+  function getAuthorizationTokenNoParam() {
+  
+    const payload = {
+      access_key: 'LdxqxWRJl5LxUKL9vAwIUANXTSkPxEqFUfiHYHix',
+      nonce: uuidv4(),
+    };
+  
+    const jwtToken = jwt.sign(payload, 'OTB2N9KwpB3sxP6gFoFJZTJfI1FlXyL8K5Blf0GC');
+  
+    console.log(jwtToken)
+    return `Bearer ${jwtToken}`;
+  }
+  
+  
+  
+  
+  const options = {
+    method: 'GET',
+    url: 'https://api.upbit.com/v1/accounts',
+    headers: {accept: 'application/json',  Authorization: getAuthorizationTokenNoParam() }
+  };
+  
+   const aaa =  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  
+  
+
+
+
+
+  
+  const onClick = async () => {
+    // try {
+    //   const exchangeRate = await getBTCExchangeRate();
+    //   console.log('Exchange Rate:', exchangeRate);
+    // } catch (error) { 
+    //   console.error('Error fetching Polygon exchange rate:', error);
+    // }
+  };
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={onClick}>
+      getPolygonExchangeRate
     </div>
   );
 }
---
+++ src/commons/api/test.js
@@ -0,0 +1,53 @@
+
+
+
+// import axios from 'axios';
+// import { v4 as uuidv4 } from "uuid";
+// import jwt from "jsonwebtoken";
+
+// export const getBTCExchangeRate = async () => {
+
+//   try {
+//     const response = await axios.get(
+
+//       'https://api.upbit.com/v1/ticker?markets=KRW-BTC',
+//     );
+//     return response.data[0].trade_price;
+//   } catch (error) {
+//     console.error('Error fetching Polygon exchange rate:', error);
+//     return false;
+//   }
+// };
+
+
+
+// function getAuthorizationTokenNoParam() {
+
+//   const payload = {
+//     access_key: 'LdxqxWRJl5LxUKL9vAwIUANXTSkPxEqFUfiHYHix',
+//     nonce: uuidv4(),
+//   };
+
+//   const jwtToken = jwt.sign(payload, 'OTB2N9KwpB3sxP6gFoFJZTJfI1FlXyL8K5Blf0GC');
+
+//   console.log(jwtToken)
+//   return `Bearer ${jwtToken}`;
+// }
+
+
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://api.upbit.com/v1/accounts',
+//   headers: {accept: 'application/json',  Authorization: getAuthorizationTokenNoParam() }
+// };
+
+// export const aaa =  axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
