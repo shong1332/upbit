51d4b76fe8440db6940467b3f737204d4296a8ba
김순홍 <kimsoonhong@gimsunhong-ui-MacBookPro.local>
2024-02-01 19:45:42 +0900

    ffff

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   
         'https://api.upbit.com/v1/ticker?markets=KRW-BTC',
       );
-      console.log("dddd")
+      console.log("ddddddd")
       return response.data[0].trade_price;
       
     } catch (error) {
